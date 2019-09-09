1d6b9810e36e96ab1c2d19a3598f5eae78b1e9c4
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-09-09 19:03:32 -0300

    Update AumentoSalario.java

--- Aumento Salarial II/com/aumentosalarial2/AumentoSalario.java
+++ Aumento Salarial II/com/aumentosalarial2/AumentoSalario.java
@@ -12,46 +12,46 @@ public class AumentoSalario {
 		int onOff = 1;
 		
 		Scanner leitor = new Scanner(System.in);
-		/*
+		
 		while(onOff == 1) {
 			System.out.println("Digite um nome para continuar, 'fim' para sair: ");
 			nomeTrabalhador = leitor.nextLine();
 				
 			if(nomeTrabalhador != "fim"){
-				System.out.println(nomeTrabalhador + ", informe o valor do seu sal·rio inicial: ");
+				System.out.println(nomeTrabalhador + ", informe o valor do seu sal√°rio inicial: ");
 				salarioAtual = leitor.nextInt();
 				
 				if(salarioAtual <= 500) {
 					trabalhador1.calculaNovoSalario(salarioAtual, nomeTrabalhador);
 				}
 				else {
-					System.out.println(nomeTrabalhador + " n„o tem direito ao aumento de 20%.");
+					System.out.println(nomeTrabalhador + " n√£o tem direito ao aumento de 20%.");
 				}
 			}else {
 				onOff = 0;
 				System.out.println("Saindo...");
 			}
 		}
-		*/
 		
-		for(;onOff == 1;) {
+		
+		/*for(;onOff == 1;) {
 			System.out.println("Digite um nome para continuar, 'fim' para sair: ");
 			nomeTrabalhador = leitor.nextLine();
 			
 			if(nomeTrabalhador != "fim"){
-				System.out.println(nomeTrabalhador + ", informe o valor do seu sal·rio inicial: ");
+				System.out.println(nomeTrabalhador + ", informe o valor do seu sal√°rio inicial: ");
 				salarioAtual = leitor.nextInt();
 					
 				if(salarioAtual <= 500) {
 					trabalhador1.calculaNovoSalario(salarioAtual, nomeTrabalhador);			
 				}else {
-					System.out.println(nomeTrabalhador + " n„o tem direito ao aumento de 20%.");
+					System.out.println(nomeTrabalhador + " n√£o tem direito ao aumento de 20%.");
 				}
 			}else {
 				onOff = 0;
 				System.out.println("Saindo...");
 			}
-		}
+		}*/
 	}
 	
 }
