d99e0ba444e378db4d55f4f0a4940141094e8e32
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-09-12 14:50:54 -0300

    Add files via upload

---
+++ Gasolina/com/gasolina/Motorista.java
@@ -0,0 +1,7 @@
+package com.gasolina;
+
+public class Motorista {
+	float capacidadeTanque;
+	float qntGasolina;
+	float despesaFinal;
+}
---
+++ Gasolina/com/gasolina/PostoGasolina.java
@@ -0,0 +1,34 @@
+package com.gasolina;
+
+import java.util.Scanner;
+
+public class PostoGasolina {
+	
+	public static float calculoDespesa(float maxCombustivel, float tanque) {
+		float refri = 3;
+		float litros = (float) ((maxCombustivel - tanque) * 2.50);
+		
+		float despesaFinal = (float) (litros + (2 * refri));
+		
+		System.out.println("\n\n" + (maxCombustivel - tanque) + " L de gasolina - " + "R$ " + litros + "\n");
+		System.out.println( "2 uni. Refrigerante(s) - " + "R$ " + (2 * refri) + "\n");
+		System.out.println("VALOR FINAL = R$ " + despesaFinal);
+		
+		return despesaFinal;
+	}
+
+	public static void main(String[] args) {
+		Motorista motorista1 = new Motorista();
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite a capacidade do seu tanque de combustível: ");
+		motorista1.capacidadeTanque = leitor.nextInt();
+		
+		System.out.println("Digite a quantidade de combustível no seu tanque: ");
+		motorista1.qntGasolina = leitor.nextInt();
+		
+		motorista1.despesaFinal = calculoDespesa(motorista1.capacidadeTanque, motorista1.qntGasolina);		
+	}
+
+}
