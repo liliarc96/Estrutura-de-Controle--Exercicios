8decb5d84d84a25588e89f3d185cbd5eb9a97c94
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2019-09-16 19:52:31 -0300

    Add files via upload

--- Aumento Salarial II/com/aumentosalarial2/AumentoSalario.java
+++ Aumento Salarial II/com/aumentosalarial2/AumentoSalario.java
@@ -1,36 +1,38 @@
 package com.aumentosalarial2;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AumentoSalario {
 
 	public static void main(String[] args) {
 
-		Trabalhador trabalhador1 = new Trabalhador();
-		
 		boolean onOff = true;
-		
+		Trabalhador trabalhador1 = new Trabalhador();
+		List<String> listaFuncionarios = new ArrayList<>();
 		Scanner leitor = new Scanner(System.in);
 		
 		
 		while(onOff) {
-			System.out.println("Digite um nome para continuar, 'fim' para sair: ");
-			trabalhador1.nome =leitor.nextLine();
-				
-			if(trabalhador1.nome != "fim"){
-				System.out.println(trabalhador1.nome + ", informe o valor do seu salário inicial: ");
-				trabalhador1.setSalario(leitor.nextInt());
-				
+			System.out.println("\nDigite um nome para continuar, 'fim' para sair: ");
+			listaFuncionarios.add(leitor.nextLine());
+			
+			System.out.println(listaFuncionarios);
+			if(listaFuncionarios.contains("fim")){
+				listaFuncionarios.remove("fim");
+				System.out.println("Saindo...");
+				onOff = false;
+			}else {
+				System.out.println("\nInforme o valor do seu salário inicial: ");
+				trabalhador1.salario = leitor.nextDouble();
+				leitor.nextLine();
 				if(trabalhador1.salario <= 500) {
 					trabalhador1.calculaNovoSalario();
+				}else {
+					System.out.println("\nVocê não tem direito ao aumento de salário!");
 				}
-				else {
-					System.out.println(trabalhador1.nome + " não tem direito ao aumento de 20%.");
-				}
-			}else {
-				onOff = false;
-				System.out.println("Saindo...");
 			}
 		}
 	}
-	
 }
--- Aumento Salarial II/com/aumentosalarial2/Trabalhador.java
+++ Aumento Salarial II/com/aumentosalarial2/Trabalhador.java
@@ -2,7 +2,6 @@ package com.aumentosalarial2;
 
 public class Trabalhador {
 	public double salario;
-	public String nome;
 	
 	public double getSalario() {
 		return salario;
@@ -10,16 +9,10 @@ public class Trabalhador {
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 	
 	public void calculaNovoSalario() {
 		double aumento = ((this.salario * 20.0)/100.0);
 		double novoSalario = (this.salario + aumento);
-		System.out.println(this.nome + " tem o valor do novo salário igual à R$ " + novoSalario + ".");
+		System.out.println("O valor do novo salário igual à R$ " + novoSalario + ".");
 	}
 }
